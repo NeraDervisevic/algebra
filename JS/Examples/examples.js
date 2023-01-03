cc2be1e53caef91adeec4b2f321aae195516081a
Nera Dervišević <dervisevic.nera@gmail.com>
2023-01-03 20:25:59 +0100

    Examples update

---
+++ JS/Examples/examples.js
@@ -0,0 +1,12 @@
+var user_1 = 'Nera';
+var user_2 = 'Luka';
+
+var userAge, userGender, userLocation;
+
+userAge = 20;
+userGender = 'Female';
+userLocation = 'Croatia';
+
+var userAge = 20,
+    userGender = 'Female',
+    userLocation = 'Croatia';
