0695bde84f5c07dc5d14b54f43f718fb74f4192c
Nera Dervišević <dervisevic.nera@gmail.com>
2023-05-23 19:40:27 +0200

    Update

--- src/App.js
+++ src/App.js
@@ -1,12 +1,38 @@
 import logo from './logo.svg';
 import './App.css';
 import Chat from './Chat.js';
+import { useState } from 'react';
 
+function randomName() {
+  const names = ["Nera","Maja","Marko","Luka","Krešo","Nina","Sara","Laura","Ivan"];
+  const name = names[Math.floor(Math.random() * names.length)];
+  return name;
+}
+
+function randomColor() {
+  return '#' + Math.floor(Math.random() * 0xFFFFFF).toString(16);
+}
 
 function App() {
+
+  const [message, setMessage] = useState([
+    {
+      text: "This is a test message!",
+      user: {
+        color: "blue",
+        username: "bluemoon"
+      }
+    }
+  ]);
+
+  const [user, setUser] = useState({
+    username: randomName(),
+    color: randomColor()
+  });
+
   return (
     <div className="App">
-      <Chat />
+      <Chat message={message} user={user} setMessage={setMessage} setUser={setUser} />
     </div>
   );
 }
--- src/Chat.js
+++ src/Chat.js
@@ -1,7 +1,33 @@
 import React from 'react';
 
-function Chat() {
-    alert('Test passed')
-}
+const Chat = ({ message }) => {
+    return (
+        <ul className='Chat-list'>
+            {message.map(m => renderChat(m))}
+        </ul>
+        );
+};
+
+function renderChat(message) {
+    const {user, text} = message;
+    const {activeUser} = user.props;
+    const messageFromMe = user.id === activeUser.id;
+    const className = messageFromMe ?
+      "Messages-message activeUser" : "Messages-message";
+    return (
+      <li className={className}>
+        <span
+          className="avatar"
+          style={{backgroundColor: user.clientData.color}}
+        />
+        <div className="Message-content">
+          <div className="username">
+            {user.clientData.username}
+          </div>
+          <div className="text">{text}</div>
+        </div>
+      </li>
+    );
+};
 
 export default Chat;
